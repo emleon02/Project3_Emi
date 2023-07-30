cfb2d799f299070e6424cc2f805921601ad6233a
emleon02 <116023200+emleon02@users.noreply.github.com>
2023-07-30 09:37:39 -0400

    Update song.cpp

--- song.cpp
+++ song.cpp
@@ -96,7 +96,7 @@ void Playlist::PrintN(int n){
         cout << "Your playlist has " << songs.size() << " songs!" << endl;
 
         for (int i = songs.size(); i > 0; i--) {
-            // FIXME for Event and mood
+            // FIXME for Event and mood - giving an exit code 139 (interrupted by signal 11: SIGSEGV)
             cout << "Title: " << songs[i]->name << endl;
             cout << "Artist: " << songs[i]->artist << endl;
             cout << "Album: " << songs[i]->album << endl << endl;
@@ -107,7 +107,7 @@ void Playlist::PrintN(int n){
         cout << "Your playlist has " << n << " songs!" << endl;
 
         for (int i = n; i > 0; i--){
-            // FIXME for Event and mood
+            // FIXME for Event and mood - giving an exit code 139 (interrupted by signal 11: SIGSEGV)
             cout << "Title: " << songs[i]->name << endl;
             cout << "Artist: " << songs[i]->artist << endl;
             cout << "Album: " << songs[i]->album << endl << endl;
